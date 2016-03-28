d5d208be22f314f8f7122165aef736deb9adb255
Nate Yacek <nathan.yacek@autotrader.com>
2016-03-28 15:35:40 -0400

    Reworked the Angular routing to provide more accurate GA tracking.

--- app.js
+++ app.js
@@ -2,36 +2,51 @@
  * Created by Nate on 10/24/2015.
  */
 var app = angular.module('bpfApp', [
-    'ngRoute',
+    'ui.router',
     'ngMaterial'
 ]);
 
 /** Routing information for the various views */
-app.config(['$routeProvider', '$mdThemingProvider', function($routeProvider, $mdThemingProvider) {
-    $routeProvider.
-        when('/home', {
+app.config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
             templateUrl: 'components/home/homeView.html'
-        }).
-        when('/about', {
+        })
+        .state('about', {
+            url: '/about',
             templateUrl: 'components/about/aboutView.html'
-        }).
-        when('/contact', {
+        })
+        .state('contact', {
+            url: '/contact',
             templateUrl: 'components/contact/contactView.html'
-        }).
-        when ('/leadership', {
+        })
+        .state('leadership', {
+            url: '/leadership',
             templateUrl: 'components/leadership/leadershipView.html'
-        }).
-        when ('/parenting', {
-        templateUrl: 'components/parenting/parentingView.html'
-        }).
-        when ('/bostonbasics', {
-        templateUrl: 'components/bostonbasics/bostonbasicsView.html'
-        }).
-        otherwise({
-           redirectTo: '/home'
-        });
+        })
+        .state('parenting', {
+            url: '/parenting',
+            templateUrl: 'components/parenting/parentingView.html'
+        })
+        .state('bostonbasics', {
+            url: '/bostonbasics',
+            templateUrl: 'components/bostonbasics/bostonbasicsView.html'
+        })
 
     $mdThemingProvider.theme('default')
         .primaryPalette('blue');
+}]);
+
+app.run(['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
+    //initialize Google Analytics
+    $window.ga('create', 'UA-74245045-2', 'auto');
+
+    //track pageview on state change
+    $rootScope.$on('$stateChangeSuccess', function (event){
+        $window.ga('send', 'pageview', $location.path());
+    });
 }]);
 
